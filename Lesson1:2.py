6abcba0ffc95d16a311034843312328ddca587e6
Karina S <kakdelaa737@gmail.com>
2025-11-02 20:46:35 +0600

    Homework 2

---
+++ Lesson1:2.py
@@ -0,0 +1,16 @@
+class Car:
+# инициализатор
+    def __init__(self, color, model):
+        self.color=color
+        self.model=model
+
+    def drive(self, location):
+        print(f' Car {self.model} is driving in {location}')
+
+car_honda= Car(color = 'Red', model= 'Honda')
+car_subaru=Car(color='green', model= 'Subaru')
+car_subaru.drive('Bishkek')
+print(car_honda)
+print(car_subaru)
+print(car_honda.color)
+print(car_subaru.color)
